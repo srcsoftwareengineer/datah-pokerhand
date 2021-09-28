15f15e699141f96684f517150c75ebc3d7bbc649
samurai <projetaty@gmail.com>
2021-09-28 00:01:16 -0300

    POKERHAND-001 initial commit

---
+++ utils/deck_factory.py
@@ -0,0 +1,22 @@
+# !/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+'''
+Created on 27 de set de 2021
+@module: utils.deck_factory
+@summary:
+@author: Sandro Regis Cardoso | Software Engineer
+@contact: src.softwareengineer@gmail.com
+'''
+
+
+class DeckFactory(object):
+    '''
+    classdocs
+    @todo: Implement factory patter to create deck of cards
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
