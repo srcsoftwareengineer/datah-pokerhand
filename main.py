cbe9a92040e87485ec068771fb5cbd73642f0d48
samurai <projetaty@gmail.com>
2021-09-28 00:01:34 -0300

    POKERHAND-001 initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# !/usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+'''
+Created on 27 de set de 2021
+@module: main
+@summary:
+@author: Sandro Regis Cardoso | Software Engineer
+@contact: src.softwareengineer@gmail.com
+'''
+
+
+def main(*args, **kwargs):
+    try:
+        print("Running main programm...")
+        return
+    except BaseException as excpt:
+        raise excpt
+    finally:
+        NotImplemented
+
+
+if __name__ == '__main__':
+    main()
